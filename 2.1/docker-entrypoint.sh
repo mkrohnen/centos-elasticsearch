45a8eabc849122dd546bdc54fcb836e54a8d63df
mkrohnen <matthias.krohnen@milesandmore.com>
2017-01-31 14:05:21 +0100

    comment chown

--- 2.1/docker-entrypoint.sh
+++ 2.1/docker-entrypoint.sh
@@ -8,11 +8,11 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 # Drop root privileges if we are running elasticsearch
-if [ "$1" = 'elasticsearch' ]; then
+#if [ "$1" = 'elasticsearch' ]; then
 	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	exec gosu elasticsearch "$@"
-fi
+#	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+#	exec gosu elasticsearch "$@"
+#fi
 
 # As argument is not related to elasticsearch,
 # then assume that user wants to run his own process,
